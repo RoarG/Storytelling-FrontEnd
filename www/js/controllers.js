54315b572d154eaefde9bdb8d0a864fbede31f97
Espen Strømjordet <espstr@stud.ntnu.no>
2015-02-26 18:21:38 +0100

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { id: 1,
+    title: 'Nidarosdomen',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    thumbnail: 'https://media.snl.no/system/images/8077/standard_nidarosdomen__e2_80_93_1_4.jpg'},
+    { id: 2,
+    title: 'Holmenkollen',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    thumbnail: 'http://img2.custompublish.com/getfile.php/2131868.92.bcqacdpwfu/holmenkollen_f_ntb_meldetjeneste_skiforeningen.jpg?return=www.langrenn.com'},
+    { id: 3,
+    title: 'Galdhøpiggen',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    thumbnail: 'http://peakbook.org/gfx/images/1/5c/jans_hpiggen.jpg/jans_hpiggen-1.jpg'},
+    { id: 4,
+    title: 'Oldemors dukkehus',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    thumbnail: 'http://media31.dimu.no/media/image/H-DF/DF.3204/7443?width=600&height=380'},
+    {  id: 5,
+    title: '17. mai på Songe',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    thumbnail: 'http://media31.dimu.no/media/image/H-DF/DF.2776/6481?width=600&height=380'},
+  ];
+  
+  $scope.remove = function(playlist) {
+	var index = $scope.playlists.indexOf(playlist)
+	$scope.playlists.splice(index, 1);
+  }
+})
+
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
