77a60733350d7623682c0821c483987763de2259
Ragnhild Krogh <rakrogh91@gmail.com>
2015-06-18 16:17:05 +0200

    Rating state, scrolling on map tab, map size, increased category circle contrast.

--- www/js/controller/RatingCtrl.js
+++ www/js/controller/RatingCtrl.js
@@ -9,7 +9,7 @@ angular.module('RatingCtrl', [])
 
 stories.controller("RatingCtrl", function($scope, Requests, $window, $ionicHistory, $cordovaDialogs) {
 	$scope.userId = $window.localStorage.getItem('userId');
-	$scope.ratingStatus = "notRated";
+	$scope.currentlyRating = false;
 
 	$scope.showAlert = function(title, msg) {
 			var alertPopup = $ionicPopup.alert({
@@ -25,16 +25,17 @@ stories.controller("RatingCtrl", function($scope, Requests, $window, $ionicHisto
 
 	// Rates story from 1-5. Clears cache so that recommendations are updated. 
 	$scope.rateFunction = function(rating) {
-		$scope.ratingStatus = "rating";
+		$scope.currentlyRating = true;
 		$scope.story.rating = rating;
             
     Requests.addRating(Requests.getSelectedStory(), $scope.userId, rating).then(function(response) {
-      $scope.ratingStatus = "rated";
+    	$scope.currentlyRating = false;
     }, function(response) {
-      
+      $scope.currentlyRating = false;
       $scope.showAlert('Vettu Hva?', "Tilbakemeldingen din ble ikke lagret.");
-      $scope.ratingStatus = "notRated";
+      
     });
+    
     //$ionicHistory.clearCache();
 	};
 })
